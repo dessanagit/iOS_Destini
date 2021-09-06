d9d493ea364e78daea6382dc300da3890493d400
dessanagit <dessanacms@gmail.com>
2021-09-06 14:55:24 -0400

    Create a Structure

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,11 +14,27 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    @IBAction func choiceMade(_ sender: UIButton) {
+    }
+    
+//    var storyTrack = 0
+    
+    let stories = [Story(title: "You see a fork in the road.", choice1: "Take a left.", choice2: "Take a right."),
+    Story(title: "You see a tiger", choice1: "Shout for help.", choice2: "Play dead."),
+    Story(title: "You find a treasure chest.", choice1: "Open it", choice2: "Check for traps.")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+      
+        updateUI()
     }
 
-
+    func updateUI() {
+        storyLabel.text = stories[0].story
+        choice1Button.setTitle(stories[0].choice1, for: .normal)
+        choice2Button.setTitle(stories[0].choice2, for: .normal)
+        
+    }
+    
 }
 
--- Destini-iOS13/Model/Story.swift
+++ Destini-iOS13/Model/Story.swift
@@ -7,3 +7,17 @@
 //
 
 import Foundation
+
+struct Story {
+    var story: String
+    var choice1: String
+    var choice2: String
+    
+    init(title: String, choice1: String, choice2: String) {
+        story = title
+        self.choice1 = choice1
+        self.choice2 = choice2
+    }
+    
+    
+}
